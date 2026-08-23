ab05788f6669a575b83d7377b901afb455aeea24
Nathalia <nathaliamend017@gmail.com>
2026-08-23 09:52:59 -0400

    primeiro exercicio testado e concluido.

---
+++ vota.js
@@ -0,0 +1,29 @@
+//var nome = 'nathalia';
+//var idade = 25; 
+
+function votacao(nome, idade) {
+    if (idade < 16) {
+        console.log(`${nome} não deverá votar esse ano.`);
+
+    } if (idade >= 16 && idade <= 70) {
+        console.log(`${nome} deverá votar esse ano.`);
+    } if (idade == 16 || idade == 17 || idade > 70) {
+        console.log(`O voto de ${nome} é opcional.`);
+    }
+
+}
+// Para atribuir valores as variaveis pelo terminal:
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+readline.question('Digite o nome: ', (nome) => {
+    console.log(`Você digitou: ${nome}`);
+    readline.question('Digite a idade: ', (idade) => {
+        console.log(`Você digitou: ${idade}`);
+        votacao(nome, idade);
+        readline.close();
+    })
+    
+});
